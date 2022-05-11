0f93eeb31981fa8b4df5ebba6131396447f8a700
noodie <1746549908@qq.com>
2022-05-11 09:46:15 +0800

    Update sample4.cs
    一定不要使用using  请求会立即触发监听线程然后释放server， 程序就崩溃了

--- aspnet/web-api/overview/older-versions/self-host-a-web-api/samples/sample4.cs
+++ aspnet/web-api/overview/older-versions/self-host-a-web-api/samples/sample4.cs
@@ -4,9 +4,15 @@ config.Routes.MapHttpRoute(
     "API Default", "api/{controller}/{id}", 
     new { id = RouteParameter.Optional });
 
+/* 不要使用using 程序会崩溃
 using (HttpSelfHostServer server = new HttpSelfHostServer(config))
 {
     server.OpenAsync().Wait();
     Console.WriteLine("Press Enter to quit.");
     Console.ReadLine();
 }
+*/
+ HttpSelfHostServer server = new HttpSelfHostServer(config);
+ server.OpenAsync().Wait();
+ Console.WriteLine("Press Enter to quit.");
+ Console.ReadLine();
